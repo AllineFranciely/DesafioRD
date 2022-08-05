2a0f3e4187e2de406dfd0ed0f8a9a44d9caf17e4
Alline Franciely <alline_franciely@hotmail.com>
2022-08-05 17:32:04 -0300

    Boddy 3

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from '../components/Header';
 import Boddy1 from '../components/Boddy1';
 import Boddy2 from '../components/Boddy2';
+import Boddy3 from '../components/Boddy3';
 
 function Home () {
   return (
@@ -9,6 +10,7 @@ function Home () {
       <Header />
       <Boddy1 />
       <Boddy2 />
+      <Boddy3 />
     </div>
   );
 }
